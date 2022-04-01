939701622b0e38fefda426ca4262b4e668de2805
Danila Bernat <danila6545@gmail.com>
2022-04-01 20:03:50 +0300

    initial commit

---
+++ Weather Now/Weather Now/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather Now
+//
+//  Created by Бернат Данила on 01.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
